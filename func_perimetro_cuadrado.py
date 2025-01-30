7acd86e8917259c1409f8e867ea75fc362aadafa
nicolassarmiento28 <109758852+nicolassarmiento28@users.noreply.github.com>
2025-01-29 22:48:55 -0300

    Create func_perimetro_cuadrado.py

---
+++ func_perimetro_cuadrado.py
@@ -0,0 +1,15 @@
+# HACER EL PROGRAMA DE AREA O PERIMETRO DE UN CUADRADO CON UNA FUNCION
+
+def perimetro_cuadrado2():
+
+    longitud_lado2 = float(input("Introduzca la longitud de un Lado del cuadrado en cm :"))
+    print (f"la longitud del ladc proporcionado es : {longitud_lado}")
+
+
+    perimetro_cuadrado2 = longitud_lado + longitud_lado + longitud_lado + longitud_lado
+
+
+
+    print(f"el resultado del perimetro del cuadrado en cm es : {perimetro_cuadrado}")
+
+perimetro_cuadrado2()
